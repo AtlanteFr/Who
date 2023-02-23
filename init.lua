4c968939042a12d25429da9af7a358573c0d4e37
Atlante <108891261+AtlanteFr@users.noreply.github.com>
2023-02-23 13:41:25 +0100

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,16 @@
+-- Définir la commande "who"
+minetest.register_chatcommand("who", {
+    description = "Displays the number of connected players",
+    func = function(name)
+        -- Récupérer la liste des joueurs connectés
+        local players = minetest.get_connected_players()
+        -- Récupérer le nombre de joueurs connectés
+        local count = #players
+        -- Afficher le nombre de joueurs connectés dans la console du serveur
+        print("There are " .. count .. " player(s) connected.")
+        -- Afficher le nombre de joueurs connectés dans le chat du joueur qui a exécuté la commande
+        if name and name ~= "" then
+            minetest.chat_send_player(name, "There are " .. count .. " player(s) connected.")
+        end
+    end,
+})
